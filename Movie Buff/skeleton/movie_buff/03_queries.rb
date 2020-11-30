997d1ead41c56f012e33bbc9b4ec3ee3a71e6403
Juan Sanchez <juansanchez721@outlook.com>
2020-11-30 16:28:03 -0500

    Started 03_queries

--- Movie Buff/skeleton/movie_buff/03_queries.rb
+++ Movie Buff/skeleton/movie_buff/03_queries.rb
@@ -2,10 +2,21 @@ def what_was_that_one_with(those_actors)
   # Find the movies starring all `those_actors` (an array of actor names).
   # Show each movie's title and id.
 
+  Movie 
+  .select(:id, :title)
+  .joins(:actors)
+  .where(actors: {name: those_actors})
+  .group(:id)
+  .having('COUNT(actors.id) = ?', those_actors.length)
 end
+  # .group_by('movies.id')
+
+# .where('castings.ord = 1')
 
 def golden_age
   # Find the decade with the highest average movie score.
+  Movie
+  .select(:yr)
 
 end
 
