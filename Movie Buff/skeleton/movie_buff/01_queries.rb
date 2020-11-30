5a888f18545e20d63cbe441f662ebbab65ad0848
Juan Sanchez <juansanchez721@outlook.com>
2020-11-30 14:43:47 -0500

    Finished 01 file specs.

--- Movie Buff/skeleton/movie_buff/01_queries.rb
+++ Movie Buff/skeleton/movie_buff/01_queries.rb
@@ -70,6 +70,10 @@ def directed_by_one_of(them)
   # Movie.where(yr: years)
   #
   # Find the id and title of all the movies directed by one of 'them'.
+  Movie
+  .select(:id, :title)
+  .joins(:director)
+  .where('actors.name IN (?)', them )
 
 end
 
@@ -85,4 +89,7 @@ def movie_names_before_1940
   #
   # Use pluck to find the title of all movies made before 1940.
 
+  Movie 
+  .where('yr < ?', 1940)
+  .pluck(:title)
 end
