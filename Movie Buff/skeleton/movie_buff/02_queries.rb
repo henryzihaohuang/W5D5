0a92d7058b35ba4999de44fa98bdcc42fca87ce7
henryzihaohuang <atlas.bb.contact@gmail.com>
2020-11-30 14:21:42 -0600

    Finish 02_queries.rb

--- Movie Buff/skeleton/movie_buff/02_queries.rb
+++ Movie Buff/skeleton/movie_buff/02_queries.rb
@@ -3,16 +3,30 @@ def eighties_b_movies
   # 3 and 5 (inclusive).
   # Show the id, title, year, and score.
 
+  Movie
+    .select(:id, :title, :yr, :score)
+    .where(yr: (1980..1989), score: (3..5))
+
 end
 
 def bad_years
   # List the years in which a movie with a rating above 8 was not released.
-
+  Movie
+    .group(:yr)
+    .having('MAX(score) < 8')
+    .pluck(:yr)
+    
 end
 
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
+  Actor
+    .select(:id, :name)
+    .joins(:movies)
+    .where('movies.title IN (?)', title)
+    .order('castings.ord')
+
 
 end
 
@@ -23,10 +37,22 @@ def vanity_projects
 
   # Note: Directors appear in the 'actors' table.
 
+  Movie
+  .select(:id, :title, 'actors.name')
+  .joins(:actors)
+  .where('actor_id = director_id  AND castings.ord = 1')
+
 end
 
 def most_supportive
   # Find the two actors with the largest number of non-starring roles.
   # Show each actor's id, name and number of supporting roles.
 
+  Actor
+  .select(:id, :name, 'COUNT(*) AS "roles"')
+  .joins(:movies)
+  .where('castings.ord!=1')
+  .group('actors.id')
+  .order('COUNT(*) DESC, actors.name')
+  .limit(2)
 end
